9234f05dc2ade5a262db0acf23c3553bb0db9726
Alqama Hasnain <alqama.hasnain@gmail.com>
2024-10-06 17:08:06 +0530

    added update profile

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { register, login, logout, editProfile } from '../controllers/authController.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/register', register);
-router.post('/profile', editProfile);
+router.post('/profile', protect, editProfile);
 router.post('/login', login);
 router.post('/logout', logout);
 
